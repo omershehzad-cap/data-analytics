ee8f46dc91aae154c3100d72ba17f08ac2cce21d
CAPRICORN\Omer.Shehzad <omer.shehzad@capricorn.coop>
2025-03-22 16:41:55 +0800

    Added MatPlotLib

--- Covid19_Analysis.py
+++ Covid19_Analysis.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-""""
+
 # Sample data
 x = [1, 2, 3, 4, 5]
 y = [10, 20, 25, 30, 40]
@@ -16,7 +16,7 @@ plt.ylabel('Y-axis')
 
 # Display the plot
 plt.show()
-"""
+
 
 # Using a raw string
 csv_file_path = r'C:\Users\Omer.Shehzad\Documents\Python\covid19_italy_region.csv'
